32d57ea7ec988a168a206a47a7cf86eb1d51915f
Zack Greenfield <zackg007@verizon.net>
2022-02-26 09:50:32 -0500

    added requirements for models and foreign key relationships

--- models/index.js
+++ models/index.js
@@ -4,13 +4,24 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.hasOne(Category, {
+  foreignKey: "category_id"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "product_id"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: "tag_id"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: "product_id"
+});
 
 module.exports = {
   Product,
