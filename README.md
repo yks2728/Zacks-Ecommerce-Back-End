614ef26b637b8cc810e7e810b7b52cf7117c1f8d
Zack Greenfield <zackg007@verizon.net>
2022-02-27 13:03:16 -0500

    added video link to README and removed commented section from product routes

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## This is my back end for an ecommerce website. I hope you like it
 
-[Link to Video]()
+[Link to Video](https://drive.google.com/file/d/1wTS23QKtw32qXe10rFeslo9sOd__fHb9/view)
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -41,14 +41,6 @@ router.get("/:id", (req, res) => {
 
 // create new product
 router.post("/", (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
