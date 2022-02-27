6c15a1595f4370085846f6fb44692904b9fb3902
Zack Greenfield <zackg007@verizon.net>
2022-02-27 09:01:46 -0500

    addeed remaining product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -127,7 +127,22 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    },
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        res.status(404).json({ message: "no post found with this id "});
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
